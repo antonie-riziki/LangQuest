9a3c8362d15d68ad39a7c952336105bb7b69120a
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-06-11 10:22:39 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+
+reg_page = st.Page("./pgs/registration.py", title="register", icon=":material/person_add:")
+signin_page = st.Page("./pgs/signin.py", title="sign in", icon=":material/login:")
+# home_page = st.Page("./pgs/main.py", title="home page", icon=":material/home:")
+td_page = st.Page("./pgs/quest.py", title="techical drawing", icon=":material/language:")
+chatbot_page = st.Page("./pgs/chatbot.py", title="chatbot", icon=":material/chat:")
+
+
+
+pg = st.navigation([reg_page, signin_page, td_page, chatbot_page])
+
+st.set_page_config(
+    page_title="LangQuest",
+    page_icon="🈯",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.echominds.africa',
+        'Report a bug': "https://www.echominds.africa",
+        'About': "Preserving and promoting Kenyan indigenous languages, culture, and heritage through a tech-driven approach."
+    }
+)
+
+pg.run()
+
+
+
